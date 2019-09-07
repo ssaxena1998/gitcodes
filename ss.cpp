e7df313e8b9e7011a53750c8860f320047befa42
ssaxena1998 <ssaxena096@gmail.com>
2019-09-07 12:36:22 +0530

    third commit

--- ss.cpp
+++ ss.cpp
@@ -5,6 +5,7 @@ int main() {
     int a = 10;
     cout<<"learning git and github";
     cout<<"\n  basics of git and github";
+    cout<<"lolol";
     cout<<a;
     // this is our first version
     //this is our second version
