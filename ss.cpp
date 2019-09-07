e16a0367bc76ce46e5c6291508e0c89b55c0cc27
ssaxena1998 <ssaxena096@gmail.com>
2019-09-07 12:29:09 +0530

    first change

---
+++ ss.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main() {
+    int a = 10;
+    cout<<"learning git and github";
+    cout<<"\n  basics of git and github";
+    cout<<a;
+    // this is our first version
+    return 0;
+}
